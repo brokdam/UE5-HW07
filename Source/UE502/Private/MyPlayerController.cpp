77cfc25aaa7ea3c849ef4e0d600b565e785acf88
BRD\brogd <brogdam208@kakao.com>
2026-04-16 15:23:19 +0900

    게임모드, 플레이어 컨트롤러, IA, IMC 생성 및 설정

---
+++ Source/UE502/Private/MyPlayerController.cpp
@@ -0,0 +1,8 @@
+#include "MyPlayerController.h"
+
+AMyPlayerController::AMyPlayerController()
+    : InputMappingContext(nullptr),
+    MoveAction(nullptr),
+    LookAction(nullptr)
+{
+}
---
+++ Source/UE502/Public/MyPlayerController.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MyPlayerController.generated.h"
+
+class UInputMappingContext; // IMC 관련 전방 선언
+class UInputAction; // IA 관련 전방 선언
+
+UCLASS()
+class UE502_API AMyPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	AMyPlayerController();
+
+	// 에디터에서 세팅할 IMC
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputMappingContext* InputMappingContext;
+	// IA_Move를 지정할 변수
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* MoveAction;
+	// IA_Look를 지정할 변수
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* LookAction;
+
+};
